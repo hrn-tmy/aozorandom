09141e409cf22cf560b3f25bbe759c6208e90c28
hrn-tmy <mj0829mj0625@icloud.com>
2026-02-27 21:40:50 +0900

    add: 実装完了

---
+++ go.mod
@@ -0,0 +1,5 @@
+module aozorandom
+
+go 1.26
+
+require golang.org/x/text v0.34.0
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"archive/zip"
+	"bytes"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/text/transform"
+
+	"golang.org/x/text/encoding/japanese"
+)
+
+const URL = "https://www.aozora.gr.jp/index_pages/list_person_all.zip"
+
+type Book struct {
+	Author    string
+	Title     string
+	Publisher string
+}
+
+func main() {
+	author := os.Args[1]
+
+	resp, err := http.Get(URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	zipReader, err := zip.NewReader(bytes.NewReader(body), int64(len(body)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	books := []Book{}
+	for _, f := range zipReader.File {
+		rc, err := f.Open()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rc.Close()
+
+		reader := transform.NewReader(rc, japanese.ShiftJIS.NewDecoder())
+		csvReader := csv.NewReader(reader)
+		csvReader.FieldsPerRecord = -1
+
+		// ヘッダー行をスキップ
+		if _, err := csvReader.Read(); err != nil {
+			log.Fatal(err)
+		}
+
+		for {
+			record, err := csvReader.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+			author := record[1]
+			title := record[3]
+			publisher := record[11]
+
+			books = append(books, Book{
+				Author:    author,
+				Title:     title,
+				Publisher: publisher,
+			})
+		}
+	}
+
+	var matched []Book
+	for _, b := range books {
+		if strings.Contains(b.Author, author) {
+			matched = append(matched, b)
+		}
+	}
+
+	if len(matched) == 0 {
+		log.Fatalf("「%s」の作品が見つかりませんでした\n", author)
+	}
+
+	pick := matched[rand.Intn(len(matched))]
+
+	fmt.Printf("作品: %s\n", pick.Title)
+	fmt.Printf("出版社: %s\n", pick.Publisher)
+}
