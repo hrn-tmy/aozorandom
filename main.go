566ad3cb2bd79d47042a7fe8c1085aff16a8c619
hrn-tmy <mj0829mj0625@icloud.com>
2026-03-01 21:48:17 +0900

    add: 作品名からもランダム検索できるよう修正

--- main.go
+++ main.go
@@ -28,10 +28,10 @@ type Book struct {
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("コマンドを入力してください。")
-		fmt.Println("例： aozora 夏目")
+		fmt.Println("例： aozora <作品名または著者名>")
 		os.Exit(1)
 	}
-	author := os.Args[1]
+	key := os.Args[1]
 
 	resp, err := http.Get(URL)
 	if err != nil {
@@ -88,18 +88,19 @@ func main() {
 
 	var matched []Book
 	for _, b := range books {
-		if strings.Contains(b.Author, author) {
+		if strings.Contains(b.Author, key) || strings.Contains(b.Title, key) {
 			matched = append(matched, b)
 		}
 	}
 
 	if len(matched) == 0 {
-		fmt.Printf("「%s」の作品が見つかりませんでした\n", author)
+		fmt.Printf("「%s」の作品が見つかりませんでした\n", key)
 		os.Exit(1)
 	}
 
 	pick := matched[rand.Intn(len(matched))]
 
+	fmt.Printf("作家: %s\n", pick.Author)
 	fmt.Printf("作品: %s\n", pick.Title)
 	fmt.Printf("出版社: %s\n", pick.Publisher)
 }
